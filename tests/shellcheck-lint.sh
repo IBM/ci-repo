75782080e98e4222aedac9c8a79e58b2eb10418c
AnthonyAmanse <ghieamanse@gmail.com>
2018-07-08 15:43:14 -0700

    Update shellcheck-lint and yaml-lint files
    This commit modifies these files to accepts arguments. The arguments 
    will be folder names. The files in the specified folders will be 
    exculded from linting.
    
    Example:
    doing `./ci-repo/tests/yaml-lint.sh containers iOS` will exclude the 
    files in containers and iOS. The files inside the folders will not go 
    through the linter.

--- tests/shellcheck-lint.sh
+++ tests/shellcheck-lint.sh
@@ -3,7 +3,13 @@
 # shellcheck disable=SC1090
 source "$(dirname "$0")"/../scripts/resources.sh
 
-if find . -name '*.sh' -print0 | xargs -n1 -0 shellcheck -x -s bash; then
+for arg; do
+  echo "-path ./$arg -prune -o"
+  FOLDERSTOPRUNE="-path ./$arg -prune -o $FOLDERSTOPRUNE"
+done
+
+# shellcheck disable=SC2086
+if find . $FOLDERSTOPRUNE -name '*.sh' -print0 | xargs -n1 -0 shellcheck -x -s bash; then
     test_passed "$0"
 else
     test_failed "$0"
--- tests/yaml-lint.sh
+++ tests/yaml-lint.sh
@@ -3,7 +3,13 @@
 # shellcheck disable=SC1090
 source "$(dirname "$0")"/../scripts/resources.sh
 
-if find . \( -name '*.yml' -o -name '*.yaml' \) -print0 | xargs -n1 -0 yamllint -c "$(dirname "$0")"/../.yamllint.yml; then
+for arg; do
+  echo "-path ./$arg -prune -o"
+  FOLDERSTOPRUNE="-path ./$arg -prune -o $FOLDERSTOPRUNE"
+done
+
+# shellcheck disable=SC2086
+if find . $FOLDERSTOPRUNE \( -name '*.yml' -o -name '*.yaml' \) -print0 | xargs -n1 -0 yamllint -c "$(dirname "$0")"/../.yamllint.yml; then
     test_passed "$0"
 else
     test_failed "$0"
